8ae218308abf4849443b734209317dfac8157284
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2015-04-19 11:49:41 -0700

    With changes to support redis in aws cloud

--- src/main/java/pso/shop/config/SessionRepositoryConfig.java
+++ src/main/java/pso/shop/config/SessionRepositoryConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.SessionRepositoryFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
@@ -25,7 +26,7 @@ public class SessionRepositoryConfig {
 	}
 
 	@Bean
-	public JedisConnectionFactory connectionFactory() {
-		return new JedisConnectionFactory();
+	public static ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
 	}
 }
--- src/main/java/pso/shop/web/CommonController.java
+++ src/main/java/pso/shop/web/CommonController.java
@@ -1,9 +1,38 @@
 package pso.shop.web;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @ControllerAdvice
 public class CommonController {
+
+    private final String machineIp;
+    private final String machineHostName;
+
+    public CommonController() {
+        String ip = "";
+        String hostname = "";
+        try {
+            InetAddress localInet = InetAddress.getLocalHost();
+            ip = localInet.getHostAddress();
+            hostname = localInet.getHostName();
+        } catch (UnknownHostException e) {
+            //Ignoring
+        }
+        this.machineIp = ip;
+        this.machineHostName = hostname;
+
+    }
+
+    @ModelAttribute
+    public void modelAttributes(Model model) {
+        model.addAttribute("machineIp", machineIp);
+        model.addAttribute("machineHostName", machineHostName);
+    }
 
 
 }
